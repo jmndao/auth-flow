584b21c1dc6fe11ee78b7ead2d3211283704a881
jmndao <jmndao@gmail.com>
2025-09-19 20:51:25 +0000

    fix: replace deprecated rollup-plugin-terser with @rollup/plugin-terser and update package-lock.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 const resolve = require('@rollup/plugin-node-resolve');
 const typescript = require('@rollup/plugin-typescript');
-const { terser } = require('rollup-plugin-terser');
+const { terser } = require('@rollup/plugin-terser');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
